c1d23258f45b99cbbe4cef56462a090783712bc3
Danisyatul <131296581+Danisyatul@users.noreply.github.com>
2024-01-12 01:48:45 +0800

    admin manage-product
    minor changes

--- admin/manage-products.php
+++ admin/manage-products.php
@@ -20,8 +20,8 @@ echo "<script>window.location.href='manage-products.php'</script>";
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Ayunae | Manage Products</title>
-        <link rel="icon" type="image/x-icon" href="assets\img\logo-nobg.png" />
+        <title>BungaBoulevard | Manage Products</title>
+        <link rel="icon" type="image/x-icon" href="assets\img\bblogo.png" />
         <!-- Custom icons-->
         <link href="css/custom-icons.css" rel="stylesheet" />
         <!-- Core theme CSS -->
