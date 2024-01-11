c7d5d4720efaf28fcf7ed196dae22f87ce751abf
Danisyatul <131296581+Danisyatul@users.noreply.github.com>
2024-01-12 01:44:08 +0800

    admin delivered order
    typo ngeh

--- admin/delivered-orders.php
+++ admin/delivered-orders.php
@@ -16,7 +16,7 @@ header('location:logout.php');
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>ABungaBoulevard | Manage Delivered Orders</title>
+        <title>BungaBoulevard | Manage Delivered Orders</title>
         <link rel="icon" type="image/x-icon" href="assets\img\bblogo.png" />
         <!-- Custom icons-->
         <link href="css/custom-icons.css" rel="stylesheet" />
