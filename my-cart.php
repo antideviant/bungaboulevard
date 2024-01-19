99a04c50c18da8dcd5601f8e1bb4c0b9acfdf10a
undeviant <119622310+antideviant@users.noreply.github.com>
2024-01-20 02:17:58 +0800

    My Cart updated

--- my-cart.php
+++ my-cart.php
@@ -27,6 +27,13 @@ echo "<script type='text/javascript'> document.location ='my-cart.php'; </script
         <link href="css/custom-icons.css" rel="stylesheet" />
         <!-- Core theme CSS -->
         <link href="css/styles.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+        <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+        <link rel="stylesheet" href="assets/css/Table-With-Search-search-table.css">
+        <link rel="stylesheet" href="assets/css/Table-With-Search.css">
         <script src="js/jquery.min.js"></script>
     </head>
     <style type="text/css"></style>
@@ -68,97 +75,86 @@ echo "<script type='text/javascript'> document.location ='my-cart.php'; </script
     }
 </style>
     <body>
-<?php include_once('includes/header.php');?>    
-
+        <?php include_once('includes/header.php');?>    
         <!-- Section-->
-        <section class="py-5">
-            <div class="container px-4  mt-5">
-     
+        <section class="py-5" style="margin-top: -50px;">
+            <div class="container px-4  mt-5" >
+                <div class="col-md-12 search-table-col" style="width: 1250px;text-align: left;"><span class="counter pull-right"></span>
+                    <div class="table-responsive table table-hover table-bordered results">
+                        <table class="table table-hover table-bordered">
+                            <h1><strong><i>My Cart</i></strong></h1><br>
+                            <thead class="bill-header cs">
+                                <tr>
+                                    <th id="trs-hd-1" class="col-lg-1" style="background: #ff9b94; width: 15%; text-align:center;">Product</th>
+                                    <th id="trs-hd-2" class="col-lg-2" style="background: #ff9b94; width: 25%; text-align:center;">Product Name</th>
+                                    <th id="trs-hd-3" class="col-lg-3" style="background: #ff9b94; width: 10%; text-align:center;">Product Size</th>
+                                    <th id="trs-hd-4" class="col-lg-2" style="background: #ff9b94; width: 20%; text-align:center;">Product Price</th>
+                                    <th id="trs-hd-5" class="col-lg-3" style="background: #ff9b94; width: 10%; text-align:center;">Quantity</th>
+                                    <th id="trs-hd-6" class="col-lg-2" style="background: #ff9b94; width: 20%; text-align:center;">Total Amount</th>
+                                    <th id="trs-hd-7" class="col-lg-2" style="background: #ff9b94; width: 10%; text-align:center;">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                            $uid=$_SESSION['id'];
+                            $ret = mysqli_query($con, "SELECT products.productName AS pname, products.productImage1 AS pimage, products.productPrice AS pprice, cart.productId AS pid, cart.id AS cartid,cart.productSize AS proisz,products.productPriceBeforeDiscount, cart.productQty 
+                                                    FROM cart 
+                                                    JOIN products ON products.id = cart.productId 
+                                                    WHERE cart.userId = '$uid'");
+                            $num=mysqli_num_rows($ret);
+                                if($num>0)
+                                {
+                            while ($row=mysqli_fetch_array($ret)) {
 
-    <div class="table-responsive">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th colspan="4"><h1><strong>My Cart</strong></h1></th>
-                </tr>
-            </thead>
-            <tr>
-                <thead>
-                    <th>Product</th>
-                    <th>Product Name</th>
-                    <th>Product Size</th>
-                    <th>Product Price</th>
-                    <th>Quantity</th>
-                    <th>Total Amount</th>
-                    <th>Action</th>
-                    
-                </thead>
-            </tr>
-            <tbody>
-<?php
-$uid=$_SESSION['id'];
-$ret = mysqli_query($con, "SELECT products.productName AS pname, products.productImage1 AS pimage, products.productPrice AS pprice, cart.productId AS pid, cart.id AS cartid,cart.productSize AS proisz,products.productPriceBeforeDiscount, cart.productQty 
-                           FROM cart 
-                           JOIN products ON products.id = cart.productId 
-                           WHERE cart.userId = '$uid'");
-$num=mysqli_num_rows($ret);
-    if($num>0)
-    {
-while ($row=mysqli_fetch_array($ret)) {
-
-?>
-
-                <tr>
-                    <td class="col-md-2"><img src="admin/productimages/<?php echo htmlentities($row['pimage']);?>" alt="<?php echo htmlentities($row['pname']);?>" width="100" height="130"></td>
-                    <td>
-                       <a href="product-details.php?pid=<?php echo htmlentities($pd=$row['pid']);?>"><?php echo htmlentities($row['pname']);?></a>
-                    </td>
-                    <td>
-                    <span><?php echo htmlentities($row['proisz']);?></span>
-                    <td>
-                           <span class="text-decoration-line-through">RM<?php echo htmlentities($row['productPriceBeforeDiscount']);?></span>
-                            <span>RM<?php echo htmlentities($row['pprice']);?></span>
-                            
-                    </td>
-                    <td><?php echo htmlentities($row['productQty']);?></td>
-                     <td>RM<?php echo htmlentities($row['productQty']*$row['pprice']);?></td>
-                    <td>
-                        <a href="my-cart.php?del=<?php echo htmlentities($row['cartid']);?>" onClick="return confirm('Are you sure you want to delete?')" class="btn-upper btn btn3">Delete</a>
-                    </td>
-                </tr>
-            
-                <?php } ?>
-    <tr>
-                    <td colspan="7" style="text-align:right;">
-<a href="shop-categories.php" class="btn-upper btn btn1">Continue Shopping</a>
-                        <a href="checkout.php" class="btn-upper btn btn2">Proceed for Checkout</a></td>
-                </tr>
-                <?php } else{ ?>
-                <tr>
-                    <td style="font-size: 18px; font-weight:bold ">Your Cart is Empty.&nbsp;
-<a href="shop-categories.php" class="btn-upper btn btn1">Continue Shopping</a>
-                    </td>
-
-                </tr>
-                <?php } ?>
-            </tbody>
-        </table>
-    </div>
-              
+                            ?>
+                                <tr>
+                                    <td class="col-md-2" style="width: 15%; text-align:center;"><img src="admin/productimages/<?php echo htmlentities($row['pimage']);?>" alt="<?php echo htmlentities($row['pname']);?>" width="100" height="130"></td>
+                                    <td style="width: 25%; text-align:center;">
+                                        <a href="product-details.php?pid=<?php echo htmlentities($pd=$row['pid']);?>"><?php echo htmlentities($row['pname']);?></a>
+                                    </td>
+                                    <td style="width: 10%; text-align:center;">
+                                        <span><?php echo htmlentities($row['proisz']);?></span>
+                                    </td>
+                                    <td style="width: 20%; text-align:center;">
+                                        <span class="text-decoration-line-through">RM<?php echo htmlentities($row['productPriceBeforeDiscount']);?></span><br>
+                                        <span>RM<?php echo htmlentities($row['pprice']);?></span> 
+                                    </td>
+                                    <td style="width: 10%; text-align:center;"><?php echo htmlentities($row['productQty']);?></td>
+                                    <td style="width: 20%; text-align:center;">RM<?php echo htmlentities($row['productQty']*$row['pprice']);?>.00</td>
+                                    <td style="width: 10%; text-align:center;">
+                                        <a href="my-cart.php?del=<?php echo htmlentities($row['cartid']);?>" onClick="return confirm('Are you sure you want to delete?')" class="btn-upper btn btn3">Delete</a>
+                                    </td>
+                                </tr>
+                                <?php } ?>
+                                <tr>
+                                    <td colspan="7" style="text-align:right;"><a href="shop-categories.php" class="btn-upper btn btn1">Continue Shopping</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="7" style="text-align:right;"><a href="checkout.php" class="btn-upper btn btn2">Proceed for Checkout</a>
+                                    </td>
+                                </tr>
+                                <?php } else{ ?>
+                                <tr>
+                                    <td colspan="7" style="font-size: 18px; font-weight:bold ">Your Cart is Empty.&nbsp;<a href="shop-categories.php" class="btn-upper btn btn1">Continue Shopping</a>
+                                    </td>
+                                </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
-
- 
-</div>
+        </div>
         </section>
-        <br></br>
-        <br></br>
-        <br></br>
         <!-- Footer-->
    <?php include_once('includes/footer.php'); ?>
         <!-- Custom core JS-->
         <script src="js/custom.bundle.min.js"></script>
         <!-- Core theme JS-->
         <script src="js/scripts.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="assets/js/Table-With-Search-search-table.js"></script>
     </body>
 </html>
 <?php } ?>
